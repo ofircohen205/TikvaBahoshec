1cf305de1918b08de5221ae830ac8c3e4a5b1109
Ofir Cohen <ofircohen2005@gmail.com>
2019-04-30 17:32:11 +0300

    fixed environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "<Random Text>",
-    authDomain: "<Random Text>",
-    databaseURL: "<Random Text>",
-    projectId: "<Random Text>",
-    storageBucket: "<Random Text>",
-    messagingSenderId: "<Random Text>"
+    apiKey: "AIzaSyBBGU_2UQuN3p_PfTlbBNOjFZz4iSAlCIg",
+    authDomain: "chatapp-fb7fa.firebaseapp.com",
+    databaseURL: "https://chatapp-fb7fa.firebaseio.com",
+    projectId: "chatapp-fb7fa",
+    storageBucket: "chatapp-fb7fa.appspot.com",
+    messagingSenderId: "157002596336"
   }
 };
 
