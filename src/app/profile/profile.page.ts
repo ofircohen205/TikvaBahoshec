c21e2ed094c5df70c5dbd777850edf75ed257811
Tom Nagar Levi <tomnale91@gmail.com>
2019-05-07 12:20:06 +0300

    profile nav changed

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -12,4 +12,22 @@ export class ProfilePage implements OnInit {
   ngOnInit() {
   }
 
+  onclick(e): void {
+
+    const AdminElement = document.getElementById('admin');
+    const supportRepElement = document.getElementById('supportRep');
+    const x = e.target.value;
+    if (x === 'admin') {
+      if (AdminElement.hidden === true) {
+        AdminElement.hidden = false;
+        supportRepElement.hidden = true;
+      }
+    } else {
+      if (supportRepElement.hidden === true) {
+        AdminElement.hidden = true;
+        supportRepElement.hidden = false;
+      }
+    }
+  }
+
 }
