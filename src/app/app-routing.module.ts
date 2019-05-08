c27c887898ea06bc76af3a741e7b594c27660219
Ofir Cohen <ofircohen2005@gmail.com>
2019-05-08 14:04:42 +0300

    added styles to story component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ const routes: Routes = [
   { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule' },
   { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
 ];
 
 @NgModule({
