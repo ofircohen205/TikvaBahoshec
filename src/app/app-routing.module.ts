6a488acde2464dea0db1e3996c630c5d9b5f94ae
saar <saarw93@gmail.com>
2019-05-08 15:54:51 +0300

    add the donate component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -9,6 +9,13 @@ export class DonateComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
+
+  moveToJgive(){
+    // the url,html tag should be called from here , how ?
+    window.open("https://www.jgive.com/new/he/ils/donation-targets/3670?currency=ILS&utm_campaign=Leave_no_Child_in_the_Dark&utm_source=mail");
+    }
 
 }
