26108197545613a8bcc7934f61cecb884a23816a
saar <saarw93@gmail.com>
2019-05-13 14:44:05 +0300

    before angular calendar

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,31 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
 
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.scss'],
+
 })
 export class CalenderComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {}
+  constructor() {
+   
+  }
+
+  ngOnInit() {
+  
+
+  }
+
 
 }
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ]
-  });
-
-  calendar.render();
-});
