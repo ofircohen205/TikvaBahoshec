703c20bdd1024d59be3efafcdae2ce0624c63f6e
Ofir Cohen <ofircohen2005@gmail.com>
2019-05-26 19:31:15 +0300

    minor changes

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -17,14 +17,14 @@ export class StoryComponent implements OnInit {
   constructor(private firestore: FirestoreService) { }
 
   @ViewChild('title') title;
-  //@ViewChild('description') description;
+  // @ViewChild('description') description;
   stories = [];
   storiesArray = [];
 
   // variables for the text editor
-  public value: string //=
-  //`<br/>
-  //כתוב על המקרה שלך כאן`;
+  public value: string; // =
+  // `<br/>
+  // כתוב על המקרה שלך כאן`;
 
   public tools: object = {
     items: ['Undo', 'Redo', '|',
@@ -64,17 +64,20 @@ export class StoryComponent implements OnInit {
   sendStory() {
     console.log(this.value);
     this.firestore.createStory(this.title.value, this.value);
-    alert("תודה רבה ששיתפת אותנו במקרה האישי שלך! במידה והעדות תאושר ניתן יהיה לראותה באתר תוך מספר ימים")
+    alert('תודה רבה ששיתפת אותנו במקרה האישי שלך! במידה והעדות תאושר ניתן יהיה לראותה באתר תוך מספר ימים');
     document.getElementById('tell-your-story').hidden = true;
 
 
   }
 
   showStories() {
-    let html= "<span></span>";
+    let html = '<span></span>';
     this.stories.forEach(story => {
       if (story.approved) {
-        html += "<ion-card><ion-card-header><ion-card-title text-right>" + story.title + "</ion-card-title></ion-card-header><ion-card-content id = 'desc' text-right >" + story.description + "</ion-card-content></ion-card>"
+        html += "<ion-card><ion-card-header><ion-card-title text-right>" +
+                story.title +
+                "</ion-card-title></ion-card-header><ion-card-content id = 'desc' text-right >" +
+                story.description + "</ion-card-content></ion-card>"
       }
     });
     console.log(html);
@@ -82,91 +85,5 @@ export class StoryComponent implements OnInit {
     document.getElementById('upload-stories').innerHTML = html;
 
   }
-
-  // manageStories() {
-  //   this.firestore.getStoriesId().subscribe(results => {
-  //     results.forEach(result => {
-  //       const id = result.payload.doc.id;
-  //       const data = result.payload.doc.data();
-  //       const timestampDate = data['date']['seconds'];   // save the date as timestamp
-  //       const stringDate = new Date(timestampDate * 1000).toDateString();  // save the date as a regular date form
-  //       // const approval = data['approved'];
-
-  //       this.storiesArray.push({stringDate, id, ...data });
-  //       console.log(this.storiesArray);
-  //     });
-
-  //     this.storiesArray.sort((s1, s2) => {
-  //       if (s1['date']['seconds'] > s2['date']['seconds']) {
-  //         return 1;
-  //       } else {
-  //         return -1;
-  //       }
-  //     });
-  //   });
-
-  // }
-
-
-  // editStory(story) {
-  //   document.getElementById('save-edit').hidden = false;
-  //   document.getElementById('defaultRTE').hidden = false;
-  //   document.getElementById('defaultRTE').className = story.id;
-  //   for (let i = 0; i < this.storiesArray.length; i++) {
-  //     if (story.id === this.storiesArray[i].id) {
-  //       this.value = this.storiesArray[i].description;  // edit the story
-  //     }
-  //   }
-  // }
-
-  // // delete the story from firebase and from the array of stories
-  // deleteStory(story) {
-  //   for (let i = 0; i < this.storiesArray.length; i++) {
-  //       if (story.id === this.storiesArray[i].id) {
-  //         for (let j = i + 1; j < this.storiesArray.length; j++) {  // the remove from the array doesn't work well
-  //             this.storiesArray[j - 1] = this.storiesArray[j];
-  //             this.storiesArray[j] = null;
-  //         }
-  //         this.firestore.removeStory(this.storiesArray[i].id);
-  //       }
-  //   }
-  // }
-
-  // // to confirm the story can be uploaded to the website
-  // confirmStory(story) {
-  //   for (let i = 0; i < this.storiesArray.length; i++) {
-  //     if (story.id === this.storiesArray[i].id) {
-  //       this.storiesArray[i].approved = true;
-  //       this.firestore.confirmStory(this.storiesArray[i].id, true);
-  //     }
-  //   }
-  // }
-
-  // // after the story was edited, we save the changes in it
-  // acceptStoryChange() {
-  //   const storyId = document.getElementById('defaultRTE').className;
-  //   let areEquals: number;
-  //   for (let i = 0; i < this.storiesArray.length; i++) {
-  //     areEquals = this.strcmp(storyId, i);
-  //     if (areEquals === 0) {
-  //       this.storiesArray[i].description = this.value;
-  //       this.firestore.editStory(this.storiesArray[i].id, this.value);
-  //       break;
-  //     }
-  //   }
-  //   alert('יש ללחוץ בטבלה על הכפתור \'אשר\' עבור העדות הרצויה');
-  //   document.getElementById('defaultRTE').hidden = true;
-  //   document.getElementById('save-edit').hidden = true;
-  // }
-
-  // // compare 2 strings
-  // private strcmp(storyId, i) {
-  //   for (let j = 0, n = 20; j < n; j++) {
-  //     if (storyId.toString().charAt(j) !== this.storiesArray[i].id.toString().charAt(j)) {
-  //       return -1;
-  //     }
-  //   }
-  //   return 0;
-  // }
 
 }
